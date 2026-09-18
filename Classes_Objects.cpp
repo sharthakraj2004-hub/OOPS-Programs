fd4d7ec02a8860819fe685525b18a4b18a26a024
Sharthak Raj <sharthakraj2004@gmail.com>
2026-09-18 09:33:49 +0530

    Create Classes_Objects.cpp

---
+++ Classes_Objects.cpp
@@ -0,0 +1,48 @@
+/*
+------------------------------------------------------------
+Program Title : Demonstration of Classes and Objects
+
+Name          : Sharthak Raj
+Roll No       : 474
+Division      : D
+SRN           : 01FE23BEC376
+Semester      : VII
+
+Concept Used  :
+- Class
+- Object
+- Data Members
+- Member Functions
+
+Description:
+This program demonstrates the basic concept of Object
+Oriented Programming by creating a class and using an
+object to access its data members and member functions.
+------------------------------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    int rollNo;
+
+public:
+    void setRollNo(int r) {
+        rollNo = r;
+    }
+
+    void display() {
+        cout << "Roll Number: " << rollNo << endl;
+    }
+};
+
+int main() {
+    Student s;
+
+    s.setRollNo(101);
+    s.display();
+
+    return 0;
+}
